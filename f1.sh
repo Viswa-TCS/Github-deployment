405e79d0d9c920c6946fbaad7b36a02fee1df2de
mattaviswanath <viswanath@gmail.com>
2024-02-07 17:27:41 +0530

    my first version

---
+++ f1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter the number1:" num1
+read -p "Enter the number2:" num2
+x=$(($num1+$num2))
+echo "The addition of the both numbers are: $x"
+
---
+++ f2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Enter the num1:" num1
+read -p "Enter the num2:" num2
+y=$(($num1-$num2))
+echo "the subtractrion of both numbers: $y"
